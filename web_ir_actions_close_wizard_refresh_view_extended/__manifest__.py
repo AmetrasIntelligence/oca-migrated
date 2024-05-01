f7a5fa7e746e8956347095b9905adee87cd0309c
mibab.heli <mibab@heliconia.in>
2024-05-01 18:05:20 +0530

    [IMP] web_ir_actions_close_wizard_refresh_view_extended : Improve the JS code and change module name.

--- web_ir_actions_close_wizard_refresh_view_extended/__manifest__.py
+++ web_ir_actions_close_wizard_refresh_view_extended/__manifest__.py
@@ -8,14 +8,14 @@
     "version": "16.0.1.0.0",
     "development_status": "Beta",
     "author": "LevelPrime srl, Nova Code, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/account-analytic",
+    "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
     "category": "Web",
     "depends": ["web"],
     "data": ["security/ir.model.access.csv"],
     "assets": {
         "web.assets_backend": [
-            "web_ir_actions_close_wizard_refresh_view/static/src/js/*.js"
+            "web_ir_actions_close_wizard_refresh_view_extended/static/src/js/*.js"
         ]
     },
     "application": False,
