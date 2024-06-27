b2a93ab97a0c9f4990a209b4503f10d4cb914003
mibab.heli <mibab@heliconia.in>
2024-06-27 12:50:31 +0530

    [ADD] stock_picking_product_barcode_report: Added module stock_picking_product_barcode_report

---
+++ stock_picking_product_barcode_report/models/res_company.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Tecnativa - Carlos Roca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class Company(models.Model):
+    _inherit = "res.company"
+
+    barcode_default_format = fields.Selection(
+        [("gs1_128", "Display GS1_128 format for barcodes")],
+        string="Method to choose the barcode formating",
+    )
