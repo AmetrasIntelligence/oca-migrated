ab9605f4a25d6d01fee037a91e71e0f0662b1a2f
mibab.heli <mibab@heliconia.in>
2024-07-04 10:44:23 +0530

    [ADD] purchase_last_price_info_extension: Added extension module for purchase_last_price_info.

---
+++ purchase_last_price_info_extension/__manifest__.py
@@ -0,0 +1,14 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Purchase Product Last Price Info Extension",
+    "version": "16.0.1.0.0",
+    "category": "Purchase Management",
+    "license": "AGPL-3",
+    "author": "AvanzOSC, Tecnativa, Odoo Community Association (OCA)",
+    "development_status": "Production/Stable",
+    "maintainers": ["LoisRForgeFlow"],
+    "website": "https://github.com/OCA/purchase-workflow",
+    "depends": ["purchase_last_price_info"],
+    "installable": True,
+}
---
+++ purchase_last_price_info_extension/models/product_template.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+# Copyright 2019 ForgeFlow S.L.
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    @api.depends("last_purchase_line_id")
+    def _compute_last_purchase_line_id_info(self):
+        for item in self:
+            po_line = item.sudo().last_purchase_line_id
+            item.last_purchase_price = po_line.price_unit
+            item.last_purchase_date = po_line.date_order
+            item.last_purchase_supplier_id = po_line.partner_id
+            item.last_purchase_currency_id = po_line.currency_id or item.currency_id
