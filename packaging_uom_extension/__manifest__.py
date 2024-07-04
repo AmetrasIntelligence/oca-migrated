2d7e13c39803de379e780166ee109d8042fcca1c
mibab.heli <mibab@heliconia.in>
2024-07-04 12:50:04 +0530

    [ADD] packaging_uom_extension: Added extension module for packaging_uom.

---
+++ packaging_uom_extension/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2015-2019 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Packaging UOM Extension",
+    "version": "16.0.1.0.0",
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "category": "Warehouse",
+    "development_status": "Production/Stable",
+    "maintainers": ["rousseldenis"],
+    "website": "https://github.com/OCA/product-attribute",
+    "summary": "Use uom in package",
+    "depends": ["packaging_uom"],
+    "license": "AGPL-3",
+    "installable": True,
+}
---
+++ packaging_uom_extension/models/product_packaging.py
@@ -0,0 +1,33 @@
+# Copyright 2015-2019 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import models
+
+
+class ProductPackaging(models.Model):
+    _inherit = "product.packaging"
+
+    def _inverse_qty(self):
+        """
+        The inverse method is defined to make the code compatible with
+        existing modules and to not break tests...
+        :return:
+        """
+        for packaging in self:
+            category_id = packaging.product_id.uom_id.category_id
+            qty = packaging.qty if packaging.qty else 1.0
+            uom_id = packaging.uom_id.search(
+                [("factor", "=", 1.0 / qty), ("category_id", "=", category_id.id)],
+                limit=1
+            )
+            if not uom_id:
+                uom_id = packaging.uom_id.create(
+                    {
+                        "name": "{} {}".format(category_id.name, qty),
+                        "category_id": category_id.id,
+                        "rounding": packaging.product_id.uom_id.rounding,
+                        "uom_type": "bigger",
+                        "factor_inv": qty,
+                        "active": True,
+                    }
+                )
+            packaging.uom_id = uom_id
