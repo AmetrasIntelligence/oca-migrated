dd61daab57dccbb54605dd0afe744bd33674d93f
mibab.heli <mibab@heliconia.in>
2024-07-04 10:44:37 +0530

    [ADD] purchase_operating_unit_extension: Added extension module for purchase_operating_unit.

---
+++ purchase_operating_unit_extension/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2015-17 ForgeFlow S.L.
+# - Jordi Ballester Alomar
+# © 2015-17 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Operating Unit in Purchase Orders Extension",
+    "summary": "Adds the concecpt of operating unit (OU) in purchase order "
+    "management",
+    "version": "16.0.1.0.0",
+    "author": "ForgeFlow, "
+    "Serpent Consulting Services Pvt. Ltd.,"
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/operating-unit",
+    "category": "Purchase Management",
+    "depends": ["purchase_operating_unit"],
+    "license": "LGPL-3",
+    "installable": True,
+}
