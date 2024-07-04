ec6dde9c78187e4f917c6341260968f6ffe17d6d
mibab.heli <mibab@heliconia.in>
2024-07-04 10:45:11 +0530

    [ADD] sales_team_operating_unit_extension: Added extension module for sales_team_operating_unit.

---
+++ sales_team_operating_unit_extension/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2016-17 ForgeFlow S.L. (http://www.forgeflow.com)
+# Copyright 2017-TODAY Serpent Consulting Services Pvt. Ltd.
+#   (<http://www.serpentcs.com>)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Sales Team Operating Unit Extension",
+    "version": "16.0.1.0.0",
+    "author": "ForgeFlow, SerpentCS, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "website": "https://github.com/OCA/operating-unit",
+    "category": "Sales",
+    "depends": ["sales_team_operating_unit"],
+    "installable": True,
+}
