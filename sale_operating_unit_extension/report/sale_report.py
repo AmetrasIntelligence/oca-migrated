802c7d6eadf5bfa3db42c581a231b56021fe8376
Bastian Guenther <Bastian.Guenther@ametras.com>
2024-07-10 17:42:10 +0200

    [REM] sale_operating_unit_extension: Moved module to oca-extensions

--- sale_operating_unit_extension/report/sale_report.py
+++
@@ -1,9 +0,0 @@
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
-
-from odoo import fields, models
-
-
-class SaleReport(models.Model):
-    _inherit = "sale.report"
-
-    operating_unit_id = fields.Many2one("operating.unit", "Operating Unit", index=True)
